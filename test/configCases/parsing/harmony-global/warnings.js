939e083ef821a8d7a614ea33014f463b97cf42dd
Ivan Kopeykin <ikopeykin@ozon.ru>
2021-09-11 22:53:10 +0300

    fix config tests

--- test/configCases/parsing/harmony-global/warnings.js
+++ test/configCases/parsing/harmony-global/warnings.js
@@ -1,3 +1,7 @@
 module.exports = [
+	[/"global" has been used, it will evaluate to undefined/],
+	[/"global" has been used, it will evaluate to undefined/],
+	[/"global" has been used, it will evaluate to undefined/],
+	[/"global" has been used, it will evaluate to undefined/],
 	[/"global" has been used, it will evaluate to undefined/]
 ];
