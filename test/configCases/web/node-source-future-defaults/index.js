8693b52a32a9c72cc4e9f4e6beb51f741fb4bb0c
Ivan Kopeykin <ikopeykin@ozon.ru>
2021-09-13 22:38:49 +0300

    add test case

---
+++ test/configCases/web/node-source-future-defaults/index.js
@@ -0,0 +1,11 @@
+it("global", () => {
+    expect(typeof global).toBe("object");
+});
+
+it("__filename", () => {
+	expect(typeof __filename).toBe("string");
+});
+
+it("__dirname", () => {
+	expect(typeof __dirname).toBe("string");
+});
