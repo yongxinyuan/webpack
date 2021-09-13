23c7ce7b555d6f3bb004569ee6e781fd060e217b
Ivan Kopeykin <ikopeykin@ozon.ru>
2021-09-13 22:54:00 +0300

    update test case

--- test/configCases/web/node-source-future-defaults/webpack.config.js
+++ test/configCases/web/node-source-future-defaults/webpack.config.js
@@ -1,7 +1,25 @@
-/** @type {import("../../../../").Configuration} */
-module.exports = {
-	target: "web",
-	experiments: {
-		futureDefaults: true
+/** @type {import("../../../../").Configuration[]} */
+module.exports = [
+	{
+		target: "web",
+		experiments: {
+			futureDefaults: true
+		}
+	},
+	{
+		target: "web",
+		node: {
+			__filename: "mock",
+			__dirname: "mock",
+			global: "warn"
+		}
+	},
+	{
+		target: "web",
+		node: {
+			__filename: "warn-mock",
+			__dirname: "warn-mock",
+			global: true
+		}
 	}
-};
+];
